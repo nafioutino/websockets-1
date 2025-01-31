34162ecc0d71d01779edadf2427b946d80c96c9f
nafioutino <nafiou531@gmail.com>
2025-01-31 12:05:08 +0100

    initialisation et configuration de tailwindcss et lucide

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex gap-5 flex-col  items-center justify-center h-screen">
+      <h2 class="text-3xl font-bold underline text-clifford  ">Home Component</h2>
+      <div class="flex justify-center items-center gap-5">
+          <Camera
+            color="blue"
+            :size="50"
+          />
+          <UserPen
+            color="purple"
+            :size="50"
+          />
+      </div>
+      <!-- <Icon :iconNode="burger" /> -->
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { Camera } from 'lucide-vue-next';
+import { UserPen } from 'lucide-vue-next';
+import { Icon } from 'lucide-vue-next';
+// import { burger } from '@lucide/lab';
+
+
+
+</script>
+
+<style></style>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import HomeComponent from '@/components/HomeComponent.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeComponent,
+    },
+  ],
+})
+
+export default router
